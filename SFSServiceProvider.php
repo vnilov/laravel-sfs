6ca65c8f23fdadc3dadb097f50497d450d8e3132
vnilov <nilov.vadim@gmail.com>
2016-09-29 16:52:09 +0300

    add config tag for publish

--- SFSServiceProvider.php
+++ SFSServiceProvider.php
@@ -4,19 +4,19 @@ namespace SimpleFileStorage;
 
 use \Illuminate\Support\ServiceProvider;
 
-class SFSServiceProvider extends ServiceProvider 
+class SFSServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->publishes([
-            __DIR__.'config/sfs.php' => config_path('sfs.php'),
-        ]);
+            __DIR__ . '/config/sfs.php' => config_path('sfs.php')
+        ], 'config');
     }
 
     public function register()
     {
         $this->app->bind('SimpleFileStorage\SFSFacade', 'SimpleFileStorage\FacadeL52');
-        $this->app->bind('SimpleFileStorage\SFS', function($app) {
+        $this->app->bind('SimpleFileStorage\SFS', function ($app) {
             return new SFSL52($app['SimpleFileStorage\SFSFacade']);
         });
     }
