8135eea32847313bd4ade886c4802216430b6a81
vnilov <nilov.vadim@gmail.com>
2016-09-30 10:40:17 +0300

    v.0.1

--- src/SFSServiceProvider.php
+++ src/SFSServiceProvider.php
@@ -13,7 +13,7 @@ class SFSServiceProvider extends ServiceProvider
         ], 'config');
 
         $this->publishes([
-            __DIR__ . '/database/migrations/' => database_path('migrations')
+            __DIR__ . '/database/migrations/sfs_create_file_table.php' => database_path('migrations/' . date('Y_m_d_His_') . 'sfs_create_file_table.php')
         ], 'migrations');
 
         $this->publishes([
--- src/database/migrations/sfs_create_file_table.php
+++ src/database/migrations/sfs_create_file_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFileTable extends Migration
+class SfsCreateFileTable extends Migration
 {
     /**
      * Run the migrations.
