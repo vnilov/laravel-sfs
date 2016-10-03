cbd0f3fb2328df6d21ba9fc8b987a7b760275848
vnilov <nilov.vadim@gmail.com>
2016-10-03 16:43:07 +0300

    add update file function

--- src/SFSFacadeL52.php
+++ src/SFSFacadeL52.php
@@ -12,17 +12,28 @@ class SFSFacadeL52 implements SFSFacade
     private $_id = null;
     private $_file = null;
 
+    /**
+     * SFSFacadeL52 constructor.
+     */
     public function __construct()
     {
         $this->_length = config('sfs.length');
         $this->_chunk_length = config('sfs.chunk_length');
     }
 
+    /**
+     * @param $id
+     */
     public function setID($id)
     {
         $this->_id = $id;
     }
 
+    /**
+     * @param UploadedFile $file
+     *
+     * @throws \Exception
+     */
     public function setFile(UploadedFile $file)
     {
         if ($file->isValid())
@@ -31,6 +42,10 @@ class SFSFacadeL52 implements SFSFacade
             throw new \Exception('Invalid file');
     }
 
+    /**
+     * @return string
+     * @throws \Exception
+     */
     public function interpolation()
     {
         if (isset($this->_id))
@@ -38,6 +53,13 @@ class SFSFacadeL52 implements SFSFacade
         throw new \Exception('Missed File ID param');
     }
 
+    /**
+     * @param null $name
+     * @param null $size
+     * @param null $type
+     *
+     * @return array
+     */
     public function setAttributes($name = null, $size = null, $type = null)
     {
         return [
@@ -47,17 +69,28 @@ class SFSFacadeL52 implements SFSFacade
         ];
     }
 
+    /**
+     * @return string
+     */
     public function getPath()
     {
         return chunk_split(substr($this->interpolation(), 0, $this->_length - $this->_chunk_length), $this->_chunk_length, "/");
     }
 
+    /**
+     * @param null $name
+     *
+     * @return string
+     */
     public function getName($name = null)
     {
         $name = (isset($name)) ? $name : $this->_file->getClientOriginalName();
         return $this->interpolation() . "." . pathinfo($name, PATHINFO_EXTENSION);
     }
 
+    /**
+     * @return mixed
+     */
     public function saveModel()
     {
         $file = new File($this->setAttributes($this->_file));
@@ -65,11 +98,20 @@ class SFSFacadeL52 implements SFSFacade
         return $file->getKey();
     }
 
+    /**
+     * @return mixed
+     */
     public function saveFile()
     {
         return $this->_file->move(storage_path(config('sfs.storage_path') . $this->getPath()), $this->getName());
     }
 
+    /**
+     * @param $id
+     *
+     * @return string
+     * @throws \Exception
+     */
     public function getUrl($id)
     {
         if ((int)$id > 0) {
@@ -80,6 +122,12 @@ class SFSFacadeL52 implements SFSFacade
         throw new \Exception('Bad ID param');
     }
 
+    /**
+     * @param UploadedFile $file
+     *
+     * @return null
+     * @throws \Exception
+     */
     public function save(UploadedFile $file)
     {
         $this->setFile($file);
@@ -87,5 +135,25 @@ class SFSFacadeL52 implements SFSFacade
         if ($this->saveFile())
             return $this->_id;
         throw new \Exception('Something went wrong when file had been saving');
+    }
+
+    /**
+     * @param              $id
+     * @param UploadedFile $file
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function update($id, UploadedFile $file)
+    {
+        if ((int)$id > 0) {
+            // set file variable
+            $this->setFile($file);
+            // receive instance
+            $file = File::findOrFail($id);
+            // update model
+            return $file->update($this->setAttributes());
+        }
+        throw new \Exception('Bad ID param');
     }
 }
