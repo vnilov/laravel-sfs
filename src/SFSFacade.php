3c4f322b32dabc8a2d0dd31758ed3e3d257c7e93
vnilov <nilov.vadim@gmail.com>
2016-10-04 10:52:46 +0300

    code refactoring

--- src/SFSFacade.php
+++ src/SFSFacade.php
@@ -2,20 +2,145 @@
 
 namespace SimpleFileStorage;
 
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+use \Illuminate\Http\UploadedFile;
 
-interface SFSFacade
+abstract class SFSFacade
 {
-    public function setID($id);
-    public function setFile(UploadedFile $file);
-    public function setAttributes($name, $size, $type);
-    public function getPath();
-    public function getName($name);
-    public function interpolation();
-    public function saveModel();
-    public function saveFile();
-    public function getUrl($id);
-    public function save(UploadedFile $file);
-    public function update($id,  UploadedFile $file);
-    public function destroy($id);
+    private $_length;
+    private $_chunk_length;
+    private $_id = null;
+    private $_file = null;
+
+    protected function __construct()
+    {
+        $this->_length = config('sfs.length');
+        $this->_chunk_length = config('sfs.chunk_length');
+    }
+
+    /**
+     * @param $id
+     */
+    protected function setID($id)
+    {
+        $this->_id = $id;
+    }
+
+    /**
+     * @return bool|null
+     */
+    protected function getID()
+    {
+        return $this->_id;
+    }
+    
+    /**
+     * @param UploadedFile $file
+     *
+     * @throws \Exception
+     */
+    protected function setFile(UploadedFile $file)
+    {
+        $this->_file = $file;
+    }
+
+    /**
+     * @return null
+     */
+    protected function getFile()
+    {
+        return $this->_file;
+    }
+    
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    protected function interpolation()
+    {
+        if (isset($this->_id))
+            return (string)sprintf("%0" . $this->_length . "d", $this->_id);
+        throw new \Exception('Missed File ID param');
+    }
+
+    /**
+     * @param null $name
+     * @param null $size
+     * @param null $type
+     *
+     * @return array
+     */
+    protected function setAttributes($name = null, $size = null, $type = null)
+    {
+        return [
+            'name' => (isset($name)) ? $name : $this->_file->getClientOriginalName(),
+            'size' => (isset($size) && (int)$type > 0) ? $size : $this->_file->getClientSize(),
+            'type' => (isset($type)) ? $type : $this->_file->getClientMimeType()
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getPath()
+    {
+        return chunk_split(substr($this->interpolation(), 0, $this->_length - $this->_chunk_length), $this->_chunk_length, "/");
+    }
+
+    /**
+     * @param null $name
+     *
+     * @return string
+     */
+    protected function getName($name = null)
+    {
+        return $this->interpolation() . "." . pathinfo(
+            (isset($name)) ?
+                $name :
+                $this->_file->getClientOriginalName(),
+            PATHINFO_EXTENSION);
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function saveModel()
+    {
+        $file = new File($this->setAttributes($this->_file));
+        $file->saveOrFail();
+        return $file->getKey();
+    }
+    
+    /**
+     * @return mixed
+     */
+    abstract protected function saveFile();
+    
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
+    abstract public function getUrl($id);
+
+    /**
+     * @param UploadedFile $file
+     *
+     * @return mixed
+     */
+    abstract public function save(UploadedFile $file);
+
+    /**
+     * @param              $id
+     * @param UploadedFile $file
+     *
+     * @return mixed
+     */
+    abstract public function update($id, UploadedFile $file);
+
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
+    abstract public function destroy($id);
 }
--- src/SFSFacadeL52.php
+++ src/SFSFacadeL52.php
@@ -3,109 +3,18 @@
 namespace SimpleFileStorage;
 
 use SimpleFileStorage\Eloquent\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+use \Illuminate\Http\UploadedFile;
 
-class SFSFacadeL52 implements SFSFacade
+class SFSFacadeL52 extends SFSFacade
 {
-    private $_length;
-    private $_chunk_length;
-    private $_id = null;
-    private $_file = null;
-
-    /**
-     * SFSFacadeL52 constructor.
-     */
-    public function __construct()
-    {
-        $this->_length = config('sfs.length');
-        $this->_chunk_length = config('sfs.chunk_length');
-    }
-
-    /**
-     * @param $id
-     */
-    public function setID($id)
-    {
-        $this->_id = $id;
-    }
-
-    /**
-     * @param UploadedFile $file
-     *
-     * @throws \Exception
-     */
-    public function setFile(UploadedFile $file)
-    {
-        if ($file->isValid())
-            $this->_file = $file;
-        else
-            throw new \Exception('Invalid file');
-    }
-
-    /**
-     * @return string
-     * @throws \Exception
-     */
-    public function interpolation()
-    {
-        if (isset($this->_id))
-            return (string)sprintf("%0" . $this->_length . "d", $this->_id);
-        throw new \Exception('Missed File ID param');
-    }
-
-    /**
-     * @param null $name
-     * @param null $size
-     * @param null $type
-     *
-     * @return array
-     */
-    public function setAttributes($name = null, $size = null, $type = null)
-    {
-        return [
-            'name' => (isset($name)) ? $name : $this->_file->getClientOriginalName(),
-            'size' => (isset($size) && (int)$type > 0) ? $size : $this->_file->getClientSize(),
-            'type' => (isset($type)) ? $type : $this->_file->getClientMimeType()
-        ];
-    }
-
-    /**
-     * @return string
-     */
-    public function getPath()
-    {
-        return chunk_split(substr($this->interpolation(), 0, $this->_length - $this->_chunk_length), $this->_chunk_length, "/");
-    }
-
-    /**
-     * @param null $name
-     *
-     * @return string
-     */
-    public function getName($name = null)
-    {
-        $name = (isset($name)) ? $name : $this->_file->getClientOriginalName();
-        return $this->interpolation() . "." . pathinfo($name, PATHINFO_EXTENSION);
-    }
-
     /**
      * @return mixed
      */
-    public function saveModel()
+    protected function saveFile()
     {
-        $file = new File($this->setAttributes($this->_file));
-        $file->saveOrFail();
-        return $file->getKey();
+        return $this->getFile()->move(storage_path(config('sfs.storage_path') . $this->getPath()), $this->getName());
     }
-
-    /**
-     * @return mixed
-     */
-    public function saveFile()
-    {
-        return $this->_file->move(storage_path(config('sfs.storage_path') . $this->getPath()), $this->getName());
-    }
-
+    
     /**
      * @param $id
      *
@@ -114,12 +23,9 @@ class SFSFacadeL52 implements SFSFacade
      */
     public function getUrl($id)
     {
-        if ((int)$id > 0) {
-            $file = File::findOrFail($id);
-            $this->setID($file->getKey());
-            return storage_path(config('sfs.storage_path') . $this->getPath()) . $this->getName($file->name);
-        }
-        throw new \Exception('Bad ID param');
+        $file = File::findOrFail($id);
+        $this->setID($file->getKey());
+        return storage_path(config('sfs.storage_path') . $this->getPath()) . $this->getName($file->name);
     }
 
     /**
@@ -133,7 +39,7 @@ class SFSFacadeL52 implements SFSFacade
         $this->setFile($file);
         $this->setID($this->saveModel());
         if ($this->saveFile())
-            return $this->_id;
+            return $this->getID();
         throw new \Exception('Something went wrong when file had been saving');
     }
 
@@ -146,28 +52,22 @@ class SFSFacadeL52 implements SFSFacade
      */
     public function update($id, UploadedFile $file)
     {
-        if ((int)$id > 0) {
-            // set file variable
-            $this->setFile($file);
-            // receive instance
-            $file = File::findOrFail($id);
-            // update model
-            return $file->update($this->setAttributes());
-        }
-        throw new \Exception('Bad ID param');
+        // set file variable
+        $this->setFile($file);
+        // receive instance
+        $file = File::findOrFail($id);
+        // update model
+        return $file->update($this->setAttributes());
     }
 
     /**
      * @param $id
-     *  
+     *
      * @return mixed
      * @throws \Exception
      */
     public function destroy($id)
     {
-        if ((int)$id > 0) {
-            return File::destroy($id);
-        }
-        throw new \Exception('Bad ID param');
+        return File::destroy($id);
     }
 }
